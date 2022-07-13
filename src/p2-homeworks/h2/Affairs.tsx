3e6e25794e16d02e0744802807e4ae725a64e136
AntohaBr <you@example.com>
2022-07-13 19:35:04 +0300

    h2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from "./Affairs.module.css";
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data:Array<AffairType>
+    setFilter:(filter:FilterType)=>void
+    deleteAffairCallback:(_id:number)=>void
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -17,16 +18,16 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {props.setFilter('all')} // need to fix
+    const setHigh = () => {props.setFilter('high')}
+    const setMiddle = () => {props.setFilter('middle')}
+    const setLow = () => {props.setFilter('low')}
 
     return (
-        <div>
-
-            {mappedAffairs}
-
+        <div className={s.someClass}>
+            <div className={s.Button}>
+                {mappedAffairs}
+            </div>
             <button onClick={setAll}>All</button>
             <button onClick={setHigh}>High</button>
             <button onClick={setMiddle}>Middle</button>
